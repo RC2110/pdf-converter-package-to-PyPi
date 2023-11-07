2d97578b250bd96df76bf4d4de4e5b25569716ba
Rajaa C <96713906+RC2110@users.noreply.github.com>
2023-11-07 08:10:59 +0530

    created a package PDf convertor (to upload in pypi.org)

---
+++ main.py
@@ -0,0 +1,11 @@
+import invoice2pdf
+
+invoice2pdf.generate(invoice_path="invoices", path_pdf="pdf_invoices",
+                     image_path="pythonhow.png",
+                     product_id="product_id",
+                     product_name="product_name", amount_purchased="amount_purchased",
+                     price_per_unit="price_per_unit", total_price="total_price")
+
+
+
+
